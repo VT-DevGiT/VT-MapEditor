d76b90c28a5575db3ebef431efe0d194254e2eed
Valentin Arthur Thomas <64769541+warquys@users.noreply.github.com>
2022-06-19 23:04:52 +0200

    Update CommandInfo.cs

--- MapEditor/Command/CommandInfo.cs
+++ MapEditor/Command/CommandInfo.cs
@@ -25,17 +25,21 @@ Warning : for some commands you need to ave selected an object !
 
 Tool : 
 The tools are revolvers, to use them you must fire them.
-You must be in edit mode to use themn, use  ""MapEditor Edit"" you must have permission ""ME.Edit"" to enter this command.
+for all tools (except the spawner), you have to pull the cursor.
+If you fire the cursor in the center you will get info on the object unless you use the destroyer.
+If you fire the colored facets to interact in one direction, example : fire whith the mover on the green face pointing up for up the schematic.
 
+You must be in edit mode to use themn, use  ""MapEditor Edit"" you must have permission ""ME.Edit"" to enter this command.
 To change the chosen schematic or increase the displacement Vector use your key {Plugin.Instance.Config.KeyUp} to Up and key {Plugin.Instance.Config.KeyDown} to Down
 
-the tools can be used without permission!
-
-note: if there are spelling mistakes it's normal, I'm a dixlexic, if necessary you can report the mistake to me so that I can correct it.
+Note : 
+- the tools can be used without permission!
+- if you don't understand, don't panic! The plugin is easy to handle. If you really don't understand go see the creator (Warkis#9984).
+- if there are spelling mistakes it's normal, I'm a dixlexic, if necessary you can report the mistake to me so that I can correct it.
 :)
 
 Credit :
-Warkis,
+Warkis for the plugin,
 AlmightyLks for the UI
 ";
             return result;
