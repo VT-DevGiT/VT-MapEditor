343627ba97c54e56fd46205edcfdcfecf2906b0d
Valentin Arthur Thomas <64769541+warquys@users.noreply.github.com>
2022-06-19 22:50:54 +0200

    Update CommandInfo.cs

--- MapEditor/Command/CommandInfo.cs
+++ MapEditor/Command/CommandInfo.cs
@@ -21,18 +21,16 @@ namespace MapEditor.Command
             result.Message = $@"Info
 Command :
 For more info ont the command enter ""MapEditor Help"".
-Warning : for som command you need to ave  selected an object ! 
+Warning : for some commands you need to ave selected an object ! 
 
 Tool : 
 The tools are revolvers, to use them you must fire them.
 You must be in edit mode to use themn, use  ""MapEditor Edit"" you must have permission ""ME.Edit"" to enter this command.
 
-To change the Amount or the slected ID of schematic use your key {Plugin.Instance.Config.KeyUp} to Up and key {Plugin.Instance.Config.KeyDown} to Down
+To change the chosen schematic or increase the displacement Vector use your key {Plugin.Instance.Config.KeyUp} to Up and key {Plugin.Instance.Config.KeyDown} to Down
 
 the tools can be used without permission!
-A lambda player can edit the map !
 
-For mort info contact Warkis#9984 on Discord. 
 note: if there are spelling mistakes it's normal, I'm a dixlexic, if necessary you can report the mistake to me so that I can correct it.
 :)
 
